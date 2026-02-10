e58b7fa44ddced254e40058b5c247b0c4b748b92
IvanMorozov <i.morozov@teplotr.ru>
2026-02-10 10:16:35 +0300

    Первичная натсройка Spring Security

--- src/main/java/com/example/bankcards/entity/User.java
+++ src/main/java/com/example/bankcards/entity/User.java
@@ -24,7 +24,7 @@ public class User {
     private String password;
     @ManyToMany
     @JoinTable(
-            name = "roles",
+            name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
